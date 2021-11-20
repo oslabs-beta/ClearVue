6bb44cf3890a943443e2cb2114984fd45397dac6
axie0320 <axie0320@gmail.com>
2021-11-20 15:58:04 -0500

    local testing with injection

---
+++ src/extension/background.js
@@ -0,0 +1,18 @@
+console.log('Hello from Background Service Worker');
+
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.contextMenus.create({
+//     id: 'sampleContextMenu',
+//     title: 'Sample Context Menu',
+//     contexts: ['selection'],
+//   });
+// });
+
+// chrome.tabs.onActivated.addListener((tab) => {
+
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.tabId },
+//     files: ['content.js'],
+//   });
+// });
---
+++ src/extension/content.js
@@ -0,0 +1,11 @@
+console.log('Hello from Content Script');
+
+console.log(window);
+
+const script = document.createElement('script');
+script.src = chrome.runtime.getURL('backend/script.js');
+
+if (document.doctype) {
+  document.documentElement.appendChild(script);
+  script.remove();
+}
