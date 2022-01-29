fc53f41287d0a36ddec812ec0816c0f97aa0b948
Nick Reardon <nickjreardon@gmail.com>
2022-01-29 13:53:14 -0500

    cleaned up extraneous components and old code from circleview

--- src/panel/views/CircleView.vue
+++ src/panel/views/CircleView.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="container-fluid">
     <div class="row gx-0">
-      <div>Render Circle Pack here</div>
-      <!-- <circle-pack></circle-pack> -->
       <zoomable-circle-pack></zoomable-circle-pack>
     </div>
   </div>
 </template>
 
 <script>
-// import CirclePack from '../components/CirclePack.vue';
 import ZoomableCirclePack from '../components/ZoomableCirclePack.vue';
 
 export default {
   components: {
-    // CirclePack,
     ZoomableCirclePack,
   },
 };
