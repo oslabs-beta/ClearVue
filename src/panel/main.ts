29da0b3c8b8bdae75c4e1f1f2258f1fa833d50e6
Nick Reardon <nickjreardon@gmail.com>
2023-06-02 20:24:44 -0400

    formatting

--- src/panel/main.ts
+++ src/panel/main.ts
@@ -5,4 +5,7 @@ import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app');
